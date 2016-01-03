658e8e47c7f13833e5be894402842c2ee831d731
Juan Martin Loyola <jmloyola@outlook.com>
2016-01-03 20:55:04 -0300

    Se agregaron datasets con los que no se trabaja en el paper original para realizar pruebas.

--- Codigo Piloto/configuracion.m
+++ Codigo Piloto/configuracion.m
@@ -1,9 +1,18 @@
 clear all; close all; clc
 
-nombreDataset = 'webkb-stemmed';
+% nombreDataset = 'webkb-stemmed';
 % nombreDataset = 'r8-all-terms'
 % nombreDataset = '20ng-stemmed';
 % nombreDataset = 'r8-all-terms-clean';
+
+%% Dataset para probar
+nombreDataset = 'PRUEBA_r8-no-short-clean';
+% nombreDataset = 'PRUEBA_r8-no-stop-clean';
+% nombreDataset = 'PRUEBA_r8-stemmed';
+% nombreDataset = 'PRUEBA_r52-all-terms-clean';
+% nombreDataset = 'PRUEBA_r52-no-short-clean';
+% nombreDataset = 'PRUEBA_r52-no-stop-clean';
+% nombreDataset = 'PRUEBA_r52-stemmed';
 
 save('configuracion.mat', 'nombreDataset');
 
--- Datasets/README.txt
+++ Datasets/README.txt
@@ -1,2 +1,4 @@
 Aqui se encuentran todos los datasets utilizados en el paper Early Text Classification.
 Los dataset se encuentran en el sitio web http://web.ist.utl.pt/acardoso/datasets/
+
+Aquellos dataset que comienzan con 'PRUEBA' fueron agregados para probar otras variaciones de R8 y otro dataset similar a R8 (R52). Estos no fueron utilizados en el paper original.
